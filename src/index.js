fc4f30860c9237ace82b939e1c375e25f8f6163f
krisbobo <kiyongbohbo@gmail.com>
2023-03-31 03:17:17 +0100

    Feat: use react router to create browser routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Math from './pages/Calculate';
+import Quote from './pages/Quote';
+import Home from './pages/Home';
 import './index.scss';
 import App from './App';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/calculator', element: <Math /> },
+      { path: '/quote', element: <Quote /> },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
