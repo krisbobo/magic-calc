e62268196a20f4924bdfe16ff75dcbde1b0e642c
krisbobo <kiyongbohbo@gmail.com>
2023-03-31 03:16:27 +0100

    Refactor: use react router to create links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
-import Calculator from './components/Calculator';
-import Quote from './components/Quote';
+import { Link, Outlet } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <Quote />
-      <Calculator />
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <header className="header">
+      <h1>Math Magician</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/calculator">Calculator</Link>
+          </li>
+          <li>
+            <Link to="/quote">Quote</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <main className="detail">
+      <Outlet />
+    </main>
+  </>
+);
 
 export default App;
